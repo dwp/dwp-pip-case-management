a796d78f05c7df32ccec9f8631ebf955c6817a37
Ryan Forbes <ryan.forbes@engineering.digital.dwp.gov.uk>
2022-09-23 11:54:21 +0100

    updates across prototype

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,12 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/filter-cases', function(req, res) {
+    if (req.body['filter-cases'] === 'caseload') {
+      res.redirect('choose-caseload');
+    } else if (req.body['filter-cases'] === 'status') {
+      res.redirect('choose-status');
+    }
+  });
+
 module.exports = router
