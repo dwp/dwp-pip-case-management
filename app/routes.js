a76a93f6e3b7bd29a06094c24bb92a5787a507c3
Ryan Forbes <ryan.forbes@engineering.digital.dwp.gov.uk>
2022-09-26 11:59:15 +0100

    Updating with new route and pages for creating a caseload

--- app/routes.js
+++ app/routes.js
@@ -3,11 +3,23 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Route for filtering case by status or case manager
+
 router.post('/filter-cases', function(req, res) {
     if (req.body['filter-cases'] === 'caseload') {
       res.redirect('choose-caseload');
     } else if (req.body['filter-cases'] === 'status') {
       res.redirect('choose-status');
+    }
+  });
+
+  // Route for assigning cases or creating a new caseload
+
+  router.post('/assign-cases', function(req, res) {
+    if (req.body['assign-to-who'] === 'jack-barnes') {
+      res.redirect('case-assigned-confirm');
+    } if (req.body['assign-to-who'] === 'create-caseload') {
+      res.redirect('create-new-caseload');
     }
   });
 
